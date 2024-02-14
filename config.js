b02509e19fe4418c5076ec21812437f3209398ba
NguyenHoaiKhuong0003 <97690304+NguyenHoaiKhuong0003@users.noreply.github.com>
2024-02-14 22:33:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Heyy Hinagum :3';
+const giftUrl = '';
+const eventName = 'Happy Valentine';
+const titleCard = 'Dear Ká';
+const contentCard = 'Hong biết hôm nay có nhận đc quà gì chưa nay tại rảnh làm chơi chơi cái thiệp này để tặng Thảo nè Happy Valentine nhé 💗💗💗';
+
+const giftImage = 'thao.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
